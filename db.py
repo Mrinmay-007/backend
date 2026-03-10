55797afc33645b4a30d4aca9c3670ab5280c6742
mrinmay2004 <mrinmay21012204@gmail.com>
2026-03-10 13:37:04 +0530

    Updated

--- db.py
+++ db.py
@@ -4,11 +4,11 @@ from sqlalchemy.orm import sessionmaker, declarative_base
 
 # Replace with your actual DB details
 # DATABASE_URL = "mysql+pymysql://root:0070100@localhost:3306/test_db"
-# DATABASE_URL = "mysql+pymysql://root:0070100@localhost:3306/attendance"
+DATABASE_URL = "mysql+pymysql://root:0070100@localhost:3306/attendance"
 
 # DATABASE_URL = "mysql+pymysql://root:ymIHbzqthbAhsLpHESxNLofBfXPqPDQT@hopper.proxy.rlwy.net:56912/railway"
 
-DATABASE_URL = "mysql+pymysql://root:pwKZoHbrXTzbSMQkzTkmEExMVGUzyIRQ@ballast.proxy.rlwy.net:40212/railway"
+# DATABASE_URL = "mysql+pymysql://root:pwKZoHbrXTzbSMQkzTkmEExMVGUzyIRQ@ballast.proxy.rlwy.net:40212/railway"
 
 
 engine = create_engine(DATABASE_URL)
