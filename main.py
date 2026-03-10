a1dd5e889ffd6e664f853dbb8794029b69430672
mrinmay2004 <mrinmay21012204@gmail.com>
2026-03-10 14:53:21 +0530

    Updated

--- main.py
+++ main.py
@@ -12,6 +12,7 @@ app = FastAPI()
 origins = [
     "http://localhost:5173",
     "http://127.0.0.1:5173",
+    'https://attnendance-app.vercel.app/',
     "https://attendance-manage-rdyu.vercel.app",
 ]
 
